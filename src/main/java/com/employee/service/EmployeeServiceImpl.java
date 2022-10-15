fd5c42ac96c961a2ba76572b7050b93b80f0ecb5
Bishnupada Maity <bishnupdm.1@gmail.com>
2022-10-15 21:11:10 +0530

    first commit

---
+++ src/main/java/com/employee/service/EmployeeServiceImpl.java
@@ -0,0 +1,24 @@
+package com.employee.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.employee.model.Employee;
+import com.employee.repository.EmployeeRepository;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	
+	@Override
+	public List<Employee> getAllEmployees() {
+		
+		return employeeRepository.findAll();
+	}
+
+}
